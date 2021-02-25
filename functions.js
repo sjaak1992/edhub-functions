7b8e691d7abd848b33bc21b7acb5a77097881543
Syakirah <syakirah@mbpvansyakirah.home>
2021-02-25 12:20:52 +0100

    uitwerkingen oefening 1tm3

--- functions.js
+++ functions.js
@@ -5,10 +5,13 @@
 // 11, 3, 9 geeft 23
 
 function addNumbers(x, y, z){
-    const addNumbers = x+y+z;
+    return x+y+z;
+
 }
 
-console.log(ad)
+const uitkomst1= addNumbers(3, 4, 5);
+const uitkomst2 = addNumbers(11, 3, 9)
+
 
 /* Opdracht 2 */
 // Schrijf een functie die twee strings verwacht en deze aan elkaar geplakt teruggeeft.
@@ -18,14 +21,33 @@ console.log(ad)
 
 
 
+function twoStringsTogether(wordOne, wordTwo){
+  return wordOne+wordTwo;
+
+
+}
+const output = twoStringsTogether("abra", "cadabra");
+console.log(output);
+
 /* Opdracht 3 */
 // Schrijf een functie die twee parameters verwacht: een woord en een karakter
 // De functie omwikkelt het woord met het karakter en geeft dit terug
 // ---- Verwachte uitkomsten:
 // "bril", "*" geeft "*bril*"
 // "beep", "_" geeft "_beep_"
-// "kaas", "Q" geeft "QkaasQ"
+// "kaas", "Q" geeft "QkaasQ"s
 
+function oefeningDrie (word, character){
+    return character + word + character;
+
+}
+
+const firstString = oefeningDrie("bril", "*");
+const secondString = oefeningDrie("beep", "_");
+const thirdString = oefeningDrie("kaas", "Q");
+
+const finalResult = oefeningDrie(firstString, secondString, thirdString);
+console.log(finalResult);
 
 
 /* Opdracht 4 */
